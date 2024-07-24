5110c40ef1acf7cc556517198c31045fae5ee936
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-24 16:22:38 +0200

    my initial setup

---
+++ lib/models/User.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: 'mentee' | 'mentor';
+  bio?: string;
+  skills?: string[];
+  createdAt: Date;
+}
+
+const UserSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    maxlength: [60, 'Name cannot be more than 60 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+  },
+  role: {
+    type: String,
+    enum: ['mentee', 'mentor'],
+    required: true,
+  },
+  bio: String,
+  skills: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
